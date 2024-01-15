39d956686a203f0264ac554f0712631f6eeb1117
dmitriyn <d.nikitin.rf@gmail.com>
2024-01-15 22:44:44 +0500

    add new scripts

---
+++ src/JavaTask3_2.java
@@ -0,0 +1,12 @@
+public class JavaTask3_2 {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        int weightKg = 98;
+        int heightCm = 187;
+        int indexBmi = service.calculate(weightKg, heightCm);
+        System.out.println(indexBmi);
+
+        //int myBonus = service.price(10000);
+        //System.out.println(myBonus);
+    }
+}
